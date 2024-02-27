2cdb70a2cce82cc87d57cb9b27a555e423c99788
Raja Khan <getrk@yahoo.com>
2024-02-26 19:29:50 -0500

    Create custom-contact-plugin.php

---
+++ wp-content/plugins/custom-contact-plugin/custom-contact-plugin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Custom Contact Plugin
+Description: A simple WordPress plugin to collect and display contact information.
+Version: 1.0
+Author: Your Name
+*/
+
+// Include necessary files
+require_once plugin_dir_path( __FILE__ ) . 'includes/database.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/shortcode.php';
