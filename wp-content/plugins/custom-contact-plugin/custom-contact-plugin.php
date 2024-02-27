0c4a91b5f194bad3c7d40f31806fad7064f185b8
Raja Khan <getrk@yahoo.com>
2024-02-27 18:29:53 -0500

    Update custom-contact-plugin.php

--- wp-content/plugins/custom-contact-plugin/custom-contact-plugin.php
+++ wp-content/plugins/custom-contact-plugin/custom-contact-plugin.php
@@ -3,9 +3,10 @@
 Plugin Name: Custom Contact Plugin
 Description: A simple WordPress plugin to collect and display contact information.
 Version: 1.0
-Author: Your Name
+Author: Raja Khan
 */
 
 // Include necessary files
 require_once plugin_dir_path( __FILE__ ) . 'includes/database.php';
 require_once plugin_dir_path( __FILE__ ) . 'includes/shortcode.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/form-handler.php';
