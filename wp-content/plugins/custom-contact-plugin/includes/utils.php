bc68af4f5fa531bd535a5c5e9d79af4954d1dbb5
Raja Khan <getrk@yahoo.com>
2024-02-27 18:29:31 -0500

    Create utils.php

---
+++ wp-content/plugins/custom-contact-plugin/includes/utils.php
@@ -0,0 +1,18 @@
+<?php
+// utils.php
+
+class Utils {
+    // Method to check if the user is logged in
+    public static function isUserLoggedIn() {
+      /*$current_user = wp_get_current_user(); 
+    
+      if ( !($current_user instanceof WP_User) ) 
+        return false; 
+    
+      echo $current_user->user_login;*/
+      return true;
+    }
+}
+
+
+
